3b88f330151a565f7296b5fc161276a37b55accc
eryshkov <wendor265>
2018-10-20 11:16:33 +0300

    AddRegistrationTableViewController added

---
+++ Hotel Manzana/AddRegistrationTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddRegistrationTableViewController.swift
+//  Hotel Manzana
+//
+//  Created by Evgeniy Ryshkov on 20/10/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class AddRegistrationTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
