9300bb17ba7e51014bbaecd48fa5aacafaf68208
eryshkov <wendor265>
2018-10-24 11:25:08 +0300

    fix grammar issue

--- Hotel Manzana/Model/Rooms.swift
+++ Hotel Manzana/Model/Rooms.swift
@@ -17,7 +17,7 @@ struct Rooms {
         rooms.append(RoomType(id: 1, name: "Single room", shortName: "Single", price: 100))
         rooms.append(RoomType(id: 2, name: "Double room", shortName: "Double", price: 200))
         rooms.append(RoomType(id: 3, name: "Family room", shortName: "Family", price: 300))
-        rooms.append(RoomType(id: 4, name: "Surerior Double room", shortName: "Surerior Double", price: 250))
+        rooms.append(RoomType(id: 4, name: "Superior Double room", shortName: "Surerior Double", price: 250))
         rooms.append(RoomType(id: 5, name: "Deluxe Single room", shortName: "Deluxe Single", price: 350))
         rooms.append(RoomType(id: 6, name: "Pent House room", shortName: "Pent House", price: 400))
     }
